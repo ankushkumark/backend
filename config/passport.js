ddce607fdf5f5acbf692041173981d1566dd607c
Ankush Kumar <gautamankush34@gmail.com>
2025-08-25 16:46:21 +0530

    first commit

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-callbackURL: "/api/auth/google/callback"
+callbackURL: "https://backend-1-bqpk.onrender.com/api/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
